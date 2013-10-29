80a3058abed3104f6b7249265caa7a4f167339b2
David Benson <dbenson@tssg.org>
2013-10-29 11:03:20 +0000

    Added Gruntfile.js. Updated packages.json with all the relevant dependencies for the test system. Added .jshintrc and .jshintignore files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,11 @@
 
 module.exports = function(grunt) {
 
+
+	var tests       = 'test/**/*_test.js'
+	var build_tests = 'build/instrument/' + tests
+	var lib         = 'lib/**/*.js'
+
   // Project configuration.
   grunt.initConfig({
     nodeunit: {
@@ -38,11 +43,26 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-testem');
+  grunt.loadNpmTasks('grunt-qunit-cov');
+  grunt.loadNpmTasks('grunt-plato');
+  grunt.loadNpmTasks('grunt-node-tap');
+  grunt.loadNpmTasks('grunt-istanbul');
+  grunt.loadNpmTasks('grunt-istanbul-coverage');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-config');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  // Default task(s).
+
+  grunt.registerTask('test',     ['nodeunit']);
+  grunt.registerTask('cover',    ['clean:build', 'instrument', 'reloadTasks', 'nodeunit', 'storeCoverage', 'makeReport']);
+  grunt.registerTask('default',  ['required',    'jshint',     'nodeunit' ]);
+  grunt.registerTask('jenkins',  ['jshint',      'cover',      'coverage',    'plato']);
+
 
 };
