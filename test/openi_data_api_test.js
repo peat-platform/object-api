38ea5907a9e4027d193c7c40b7310ee9d912aaf4
David Benson <dbenson@tssg.org>
2013-11-05 15:14:43 +0000

    Exported all of the functions in main.js so they can be unit tested in isolation. Updated the test to cover the getName and getAction functions.

--- test/openi_data_api_test.js
+++ test/openi_data_api_test.js
@@ -23,14 +23,65 @@ var openi_data_api = require(base_path + '../lib/main.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
+exports['testGetName'] = {
   setUp: function(done) {
     // setup here
-    done();
+    this.testCorrectName    = "/test/name/here"
+    this.testIncorrectName  = "/wrongformat"
+    done()
   },
-  'no args': function(test) {
+  'incorrect format name': function(test) {
     // tests here
-    test.equal('awesome', 'awesome', 'should be awesome.');
-    test.done();
+    var actual = openi_data_api.getName(this.testIncorrectName)
+
+    test.equal(actual, null, "should return null")
+    test.done()
   },
+  'correct format name'  : function(test) {
+    // tests here
+    var actual = openi_data_api.getName(this.testCorrectName)
+
+    test.notEqual(actual, null,   "should return an object")
+    test.equal   (actual, "here", "string returned not expected")
+    test.done()
+  }
 };
+
+exports['testGetAction'] = {
+    setUp: function(done) {
+        // setup here
+        done()
+    },
+    'correct format path, get method'   : function(test) {
+        // tests here
+        var testInputPath = "this is a get request"
+        var actual        = openi_data_api.getAction(testInputPath)
+
+        test.equals(actual, "GET", "should be the HTTP GET method.")
+        test.done();
+    },
+    'correct format path, put method'   : function(test) {
+        // tests here
+        var testInputPath = "this is a put request"
+        var actual        = openi_data_api.getAction(testInputPath)
+
+        test.equals(actual, "PUT", "should be the HTTP PUT method.")
+        test.done();
+    },
+    'correct format path, echo method'  : function(test) {
+        // tests here
+        var testInputPath = "this is a echo request"
+        var actual        = openi_data_api.getAction(testInputPath)
+
+        test.equals(actual, "ECHO", "should be the HTTP ECHO method.")
+        test.done();
+    },
+    'incorrect format path'  : function(test) {
+        // tests here
+        var testInputPath = "this is a request without a method"
+        var actual        = openi_data_api.getAction(testInputPath)
+
+        test.equals(actual, null, "should have returned null")
+        test.done();
+    }
+}
